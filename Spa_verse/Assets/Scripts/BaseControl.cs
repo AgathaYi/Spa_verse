88cf068c8ee20752a8f2ebd1f0803e70b8d61238
AgathaYi <tjsfid1030@naver.com>
2025-05-03 23:09:45 +0900

    feat : Player 이동 구현 중
    스크립트 작성중

--- Spa_verse/Assets/Scripts/BaseControl.cs
+++ Spa_verse/Assets/Scripts/BaseControl.cs
@@ -9,7 +9,13 @@ public class BaseControl : MonoBehaviour // 생명체들의 부모
     // private : 소문자시작.
 
     protected Rigidbody2D Rigidbody; // 스크립트에서 사용 가능해지도록 protected로 설정, 외부에서 직접 접근 불가(캡슐화)
-    
+
+    // 캐릭터 플립
+    [SerializeField] private SpriteRenderer CharacterFlip;
+
+    // Move / 이동방향, 플레이어 - wasd, 적 - 자동랜덤
+    protected Vector2 moveDirection = Vector2.zero;
+    public Vector2 MoveDirection { get => moveDirection; } // 외부 접근 경로
 
 
 
@@ -18,13 +24,31 @@ public class BaseControl : MonoBehaviour // 생명체들의 부모
         Rigidbody = GetComponent<Rigidbody2D>(); // Rigidbody2D 컴포넌트 가져오기
     }
 
-    void Start()
+    void Start() // 시작시 한번 호출, Awake() 다음에 호출
     {
         
     }
 
-    void Update()
+    void Update() // 매 프레임마다 호출, 물리 X, 일반 
     {
-        
+        // 플레이어 이동 - > playerControl 자식에서.. 업데이트가 오버라이드가 안됨.음..
+        KeyControl();
+    }
+
+    protected virtual void KeyControl()
+    {
+        // 적은 X.. 플레이어만  / 버츄얼해야 자식에서 오버라이드 가능
+    }
+
+    protected virtual void FixedUpdate() // 물리적 업데이트, 매 프레임마다 호출
+    {
+        Move(moveDirection);
+    }
+
+    // 이동 속도, 실제 이동
+    private void Move(Vector2 direction)
+    {
+        direction = direction * 7;
+        Rigidbody.velocity = direction;
     }
 }
--- Spa_verse/Assets/Scripts/PlayerControl.cs
+++ Spa_verse/Assets/Scripts/PlayerControl.cs
@@ -4,5 +4,14 @@ using UnityEngine;
 
 public class PlayerControl : BaseControl
 {
-    
+    private Camera mainCamera;
+
+    protected override void KeyControl()
+    {
+        // 플레이어 move 방법
+        if (Input.GetKey(KeyCode.W))
+        {
+            Debug.Log("W");
+        }
+    }
 }
